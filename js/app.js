b195e4d6f7f20c2151ff8c59369a1f7887066251
Nils Kriedner <nkriedner@googlemail.com>
2024-01-20 00:18:35 +0100

    Add js for notifications and bell-icon

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 // SELECT ELEMENTS
 const closeX = document.querySelectorAll(".close-x");
+const bellIcon = document.querySelector(".bell-icon svg");
+const notifictions = document.querySelector(".notifications");
+const greenAlert = document.querySelector(".green-alert");
 
 // EVENT LISTENERS
-// Delete parent element of close-x on click
+bellIcon.addEventListener("click", () => {
+    notifictions.style.display = "block";
+    greenAlert.classList.remove("green-alert");
+});
+// Close-x button (deletes parent element on click)
 for (let i = 0; i < closeX.length; i++) {
     closeX[i].addEventListener("click", () => {
         closeX[i].parentNode.style.display = "none";
