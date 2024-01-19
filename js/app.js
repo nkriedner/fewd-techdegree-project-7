34f59077ccf2b64524b8a8a05e3d163801503562
Nils Kriedner <nkriedner@googlemail.com>
2024-01-19 22:11:33 +0100

    Add closing functionality for alert message

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log("test");
+// SELECT ELEMENTS
+const closeX = document.querySelector(".close-x");
+const alertMessage = document.querySelector(".alert-message");
+
+// EVENT LISTENERS
+closeX.addEventListener("click", () => {
+    alertMessage.style.display = "none";
+});
