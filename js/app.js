25d73cdabd72434f6760cafcc1e02c02bbc008d1
Nils Kriedner <nkriedner@googlemail.com>
2024-01-19 23:56:16 +0100

    Add notification messages

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // SELECT ELEMENTS
-const closeX = document.querySelector(".close-x");
-const alertMessage = document.querySelector(".alert-message");
+const closeX = document.querySelectorAll(".close-x");
 
 // EVENT LISTENERS
-closeX.addEventListener("click", () => {
-    alertMessage.style.display = "none";
-});
+// Delete parent element of close-x on click
+for (let i = 0; i < closeX.length; i++) {
+    closeX[i].addEventListener("click", () => {
+        closeX[i].parentNode.style.display = "none";
+    });
+}
